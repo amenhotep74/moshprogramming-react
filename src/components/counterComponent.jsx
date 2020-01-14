0fc2238fc8480ea08242bb46caf4bd5dd2b973d5
unknown <Blewie144@gmail.com>
2020-01-14 18:34:21 +1030

    rendering lists

---
+++ src/components/counterComponent.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        count: 0,
+        tags: ['tag1', 'tag2', 'tag3', ""]
+    };
+
+    // if the array is empty 
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+    }
+
+    render() { 
+        return (
+        <React.Fragment>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button className="btn btn-secondary btn-sm">Increment</button>
+            <ul>
+                {/* Pull each array element from the array and map it into an li */}
+                
+            </ul>
+        </React.Fragment>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        if (count === 0) {
+            return <h1>Zero</h1>;
+        } else {
+            return this.state.count;
+        }
+    }
+
+
+}
+ 
+export default Counter;
