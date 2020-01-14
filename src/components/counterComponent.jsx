0474b4327025540d806f7333a33c055439f397af
unknown <Blewie144@gmail.com>
2020-01-15 10:08:41 +1030

    conditional rendering

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,25 +3,20 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: ['tag1', 'tag2', 'tag3', ""]
+        tags: []
     };
 
     // if the array is empty 
     renderTags() {
         if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-        return { this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     }
 
     render() { 
         return (
         <React.Fragment>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment</button>
-            <ul>
-                {/* Pull each array element from the array and map it into an li */}
-                
-            </ul>
+            { this.state.tags.length === 0 && 'Please create a new tag'}
+            { this.renderTags() }
         </React.Fragment>
         );
     }
