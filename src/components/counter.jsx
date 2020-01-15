2f2944610f4073a6eb6bd580a3cf90310ecf8413
unknown <Blewie144@gmail.com>
2020-01-15 17:05:08 +1030

    event raising

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,34 +2,37 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
+        value: this.props.value,
     };
 
     handleIncrement = () => {
-        this.setState({ count: this.state.count + 1 });
+        this.setState({ value: this.state.value + 1 });
     }
 
     render() { 
+        console.log("props", this.props);
         return (
         <div>
+            {this.props.children}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
             <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+            <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
         </div>
         );
     }
 
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
     formatCount() {
-        const { count } = this.state;
+        const { value: count } = this.state;
         if (count === 0) {
             return <h1>Zero</h1>;
         } else {
-            return this.state.count;
+            return this.state.value;
         }
     }
 
